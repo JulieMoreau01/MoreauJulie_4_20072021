b082f25607c81d6a4dbdc724a297e8344c5a5a94
Julie Moreau <julie_bonnet_01@hotmail.com>
2021-07-22 10:31:04 +0200

    JS error message Form

--- starterOnly/public/js/modal.js
+++ starterOnly/public/js/modal.js
@@ -1,3 +1,4 @@
+// FONCTION OUVERTURE FERMETURE DE LA NAV EN VERSION TELEPHONE
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -12,6 +13,16 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBtnClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const firstInput = document.getElementById("first");
+const lastInput = document.getElementById("last");
+const error = document.getElementsByClassName("error");
+const radioInput1 = document.getElementById("location1");
+const radioInput2 = document.getElementById("location2");
+const radioInput3 = document.getElementById("location3");
+const radioInput4 = document.getElementById("location4");
+const radioInput5 = document.getElementById("location5");
+const radioInput6 = document.getElementById("location6");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,3 +39,20 @@ modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// Fonction de validation du formulaire
+function validateForm() {
+    if (firstInput.value.length < 3 ) {
+      document.getElementById("error-first").innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+      return false;
+    } else if (lastInput.value.length < 3 ) {
+      document.getElementById("error-last").innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      return false;
+    } else if ((radioInput1.checked || radioInput2.checked || radioInput3.checked || radioInput4.checked || radioInput5.checked || radioInput6.checked) == false) {
+      document.getElementById("error-radio").innerText = "Une ville est obligatoire";
+      return false;
+    } else {
+      document.getElementsByTagName("main").innerText = "Thank you for submitting your registration details";
+      return true;
+    }
+};
